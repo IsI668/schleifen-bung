7f16663e5bed8b2b93f00ade15bae1102662a7aa
ismail.a8 <ismail.a8@P-0G-0B005-0393>
2022-01-26 13:51:35 +0100

    Projektdateien hinzufügen.

---
+++ schleifen übung/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+namespace schleifen_übung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a= 50;
+
+            for (int i =50;  i>= 0; i--)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
